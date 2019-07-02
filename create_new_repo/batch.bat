8f39757fb9d37eb80a4e19cea8720917261a1ec3
Ashreya Kumar Khatua <lipun0147@gmail.com>
2019-07-02 18:11:03 +0530

    User input updated
    Username and password entered by user.
    Password is masked.

--- create_new_repo/batch.bat
+++ create_new_repo/batch.bat
@@ -1,12 +1,20 @@
 @echo off
-cd "Path to repos folder" 
+setlocal
+
+cd "C:\Users\USER\Desktop\GitHub Repos"
+set /p username=Enter GitHub username:
+set /P "=_" < NUL > "Enter password"
+findstr /A:1E /V "^$" "Enter password" NUL > CON
+del "Enter password"
+set /P "password="
+cls
+color 07
 set /p repoName=Name of repository:
-python py_new_repo.py %repoName%
+python gitrepo.py %repoName% %username% %password%
 mkdir %repoName%
 cd %repoName%
 git init
-::Enter username in remote url
-git remote add origin https://github.com/username/%repoName%.git
+git remote add origin https://github.com/Ashreya12/%repoName%.git
 type nul > readme.md
 git add .
 git commit -m "Initial Commit"
--- create_new_repo/py_new_repo.py
+++ create_new_repo/py_new_repo.py
@@ -1,22 +1,23 @@
 import sys
 from selenium import webdriver
 
-name = sys.argv[1]
+reponame = sys.argv[1]
+username = sys.argv[2]
+password = sys.argv[3]
 
-# Enter path to chromedriver.exe
-browser = webdriver.Chrome(executable_path="path to chromedriver.exe")
+browser = webdriver.Chrome(executable_path="C:\Drivers\chromedriver.exe")
 browser.get('http://github.com/login')
 
 login_user = browser.find_elements_by_xpath("//*[@id='login_field']")[0]
-login_user.send_keys('username')  # Enter GitHub username
+login_user.send_keys(username)
 pass_user = browser.find_elements_by_xpath("//*[@id='password']")[0]
-pass_user.send_keys('password')  # Enter GitHub password
+pass_user.send_keys(password)
 login = browser.find_elements_by_xpath(
     "//*[@id='login']/form/div[3]/input[4]")[0]
 login.click()
 browser.get('https://github.com/new')
 repo = browser.find_elements_by_xpath("//*[@id='repository_name']")[0]
-repo.send_keys(name)
+repo.send_keys(reponame)
 repo_button = browser.find_element_by_css_selector("button.first-in-line")
 repo_button.submit()
 # browser.quit()
